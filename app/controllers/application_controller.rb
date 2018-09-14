131bfb03e2a477c431ea2076de85d3e3fe2f4418
Sean Lynch <sean.lynch@thrivecommerce.com>
2018-09-14 09:36:31 -0400

    created non-atomic commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::API
+
+	respond_to :json
+
+	# before_action :set_default_response_format
+
+	# protected 
+
+	# def set_default_response_format
+	# 	request.format = :json 
+	# end
+end
